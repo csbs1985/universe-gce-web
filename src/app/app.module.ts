e321f496bc1261331527f9d6ef8a41e46c6bfac4
Charles Santos <csbs.conta@outlook.com>
2022-08-05 19:12:34 -0300

    [fonts] adicionar e estruturar fonts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentModule } from './component/component.module';
+import { PageModule } from './page/page.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ComponentModule],
+  imports: [BrowserModule, AppRoutingModule, ComponentModule, PageModule],
   providers: [],
   bootstrap: [AppComponent],
 })
